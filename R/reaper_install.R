11927035bb69fb6e8d1d9cf1cf34f0b4cb653a3b
rpuggaardrode <r.puggaard@phonetik.uni-muenchen.de>
2024-12-17 17:38:31 +0100

    failsafe reaper installation function

---
+++ R/reaper_install.R
@@ -0,0 +1,32 @@
+#' Attempt installation of REAPER
+#'
+#' This function can be run if the installation of REAPER failed when
+#' installing the package.
+#'
+#' @return Used for side effects.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' reaper_install()
+#' }
+reaper_install <- function() {
+  curwd <- getwd()
+  on.exit(setwd(curwd))
+  dest <- paste0(system.file('bin', package='reapeR'))
+  if (dir.exists(dest)) stop('REAPER already installed')
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  setwd(dest)
+  system('git clone https://github.com/google/REAPER.git')
+  setwd('REAPER')
+  dir.create('build')
+  setwd('build')
+  system('cmake ..')
+  system('make')
+  exec <- 'reaper'
+  if (Sys.info()['sysname'] == 'Windows') exec <- paste0(exec, '.exe')
+  file.rename(exec, paste0(dest, '/', exec))
+  setwd(dest)
+  unlink('REAPER', recursive = TRUE, force = TRUE)
+  setwd(curwd)
+}
